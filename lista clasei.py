371744ad1bec29dafd925b7c5b27258e16522223
Artiom12341 <70652258+Artiom12341@users.noreply.github.com>
2021-01-26 23:57:54 +0200

    Update lista clasei.py

--- lista clasei.py
+++ lista clasei.py
@@ -1,15 +1,8 @@
 with open ('lista clasei.txt' ,'r') as f:
     x=list(eval(f.readline()))
 x.sort()
-v=''
-i=str
-while i!='SFARSIT':
-    for i in x:
-        v=v+str(i)
-        v=v+str(' ')
-        with open ('lista clasei.2.txt','w') as f:
-            f.write(v)
-            f.write('\n')
-    else :
-        break
-        
+with open ('lista clasei.2.txt','w') as f:
+   for i in range(0,len(x)):
+        f.write(str(x[i]))
+        f.write('\n')
+   
